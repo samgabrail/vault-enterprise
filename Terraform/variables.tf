352fbc41b6856ca979907ed4b471f3d464abfa1a
Sam Gabrail <sam.gabrail@tekanaid.com>
2020-07-03 15:08:19 -0400

    fixed zone issue

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -14,9 +14,9 @@ variable "instance_names" {
   "vault-prim-uswest1-b": "us-west1-b",
   "vault-prim-uswest1-c": "us-west1-c", 
   "vault-dr-prim-uswest1-a": "us-west1-a",
-  "vault-pr-useast1-a": "us-east1-a",
-  "vault-dr-prim-useast1-b": "us-east1-b",
-  "vault-dr-pr-useast1-c": "us-east1-c"}
+  "vault-pr-useast1-a": "us-east1-b",
+  "vault-dr-prim-useast1-b": "us-east1-c",
+  "vault-dr-pr-useast1-c": "us-east1-d"}
 }
 
 variable "machine_type" {
