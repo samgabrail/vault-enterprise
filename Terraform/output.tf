29478b7a241da3fb104ff99364f511ba2dfde0cd
Sam Gabrail <sam.gabrail@tekanaid.com>
2020-07-03 14:49:51 -0400

    update to output again

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,4 +1,4 @@
 # Outputs file
 output "vault_url" {
-  value = "http://${google_compute_instance.vault-servers[var.instance_names.key].network_interface.0.access_config.0.nat_ip}:8200 to access ${each.key}"
+  value = "http://${google_compute_instance.vault-servers[var.instance_names.key].network_interface.0.access_config.0.nat_ip}:8200 to access ${var.instance_names.key}"
 }
