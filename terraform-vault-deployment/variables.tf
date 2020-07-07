66ae5653142b089bc28eb641c4b63f25ce7f84a7
Sam Gabrail <sam.gabrail@tekanaid.com>
2020-07-07 15:51:08 -0400

    added a node for testing raft snapshot

--- terraform-vault-deployment/variables.tf
+++ terraform-vault-deployment/variables.tf
@@ -16,7 +16,8 @@ variable "instance_names" {
   "vault-dr-prim-uswest1-a": "us-west1-a",
   "vault-pr-useast1-b": "us-east1-b",
   "vault-dr-prim-useast1-c": "us-east1-c",
-  "vault-dr-pr-useast1-d": "us-east1-d"}
+  "vault-dr-pr-useast1-d": "us-east1-d",
+  "test-raft-snapshot": "us-east1-d"}
 }
 
 variable "machine_type" {
