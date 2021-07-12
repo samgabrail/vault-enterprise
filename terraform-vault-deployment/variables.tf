87d9a5f4645965643ec5beab77989464494b1c36
samgabrail <sameh.gabrail@gmail.com>
2021-07-12 15:01:05 -0400

    RAFT over 2 AZs

--- terraform-vault-deployment/variables.tf
+++ terraform-vault-deployment/variables.tf
@@ -7,17 +7,29 @@ variable "location" {
   default     = "us-central1-a"
 }
 
+// variable "instance_names" {
+//   type = map(string)
+//   default = {
+//   "vault-prim-uswest1-a": "us-west1-a",
+//   "vault-prim-uswest1-b": "us-west1-b",
+//   "vault-prim-uswest1-c": "us-west1-c", 
+//   "vault-dr-prim-uswest1-a": "us-west1-a",
+//   "vault-pr-useast1-b": "us-east1-b",
+//   "vault-dr-prim-useast1-c": "us-east1-c",
+//   "vault-dr-pr-useast1-d": "us-east1-d",
+//   "test-raft-snapshot": "us-east1-d"}
+// }
+
 variable "instance_names" {
   type = map(string)
   default = {
-  "vault-prim-uswest1-a": "us-west1-a",
-  "vault-prim-uswest1-b": "us-west1-b",
-  "vault-prim-uswest1-c": "us-west1-c", 
-  "vault-dr-prim-uswest1-a": "us-west1-a",
-  "vault-pr-useast1-b": "us-east1-b",
-  "vault-dr-prim-useast1-c": "us-east1-c",
-  "vault-dr-pr-useast1-d": "us-east1-d",
-  "test-raft-snapshot": "us-east1-d"}
+  "vault1-uswest1-a": "us-west1-a",
+  "vault2-uswest1-a": "us-west1-a",
+  "vault3-uswest1-a": "us-west1-a", 
+  "vault4-uswest1-a": "us-west1-a",
+  "vault5-uswest1-b": "us-west1-b",
+  "vault6-uswest1-b": "us-west1-b",
+  "vault7-uswest1-b": "us-west1-b",
 }
 
 variable "machine_type" {
